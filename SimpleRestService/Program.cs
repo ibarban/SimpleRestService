249849de7e5bf18e48204619c6e8435f5a2d2e83
Ivan Barbancic <ibarbancic@microsoft.com>
2024-12-22 13:54:10 +0100

    Remove some code

--- SimpleRestService/Program.cs
+++ SimpleRestService/Program.cs
@@ -2,21 +2,9 @@ long requestCount = 0;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.MapGet("/helath", (HttpContext context) => {
+app.Map("/health", (HttpContext context) => {
     if (requestCount < 500)
     {
         context.Response.StatusCode = 200; // OK
